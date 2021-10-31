384d850c8af313e718d2923a24de44f996120ddf
dev <vytorfelix2@gmail.com>
2021-10-31 11:55:04 -0300

    refact: split the responsibilities of SideBar and Content into two new components with respective names

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,32 @@
-export function SideBar() {
-  // Complete aqui
+import { GenreResponseProps } from '../App';
+import { Button } from './Button';
+
+interface SideBarProps {
+  genres: GenreResponseProps[],
+  selectedGenreId: number,
+  handleClickButton: (id: number) => void;
+}
+
+export function Sidebar({genres, selectedGenreId, handleClickButton}: SideBarProps) {
+  function handleClick(id: number) {
+    handleClickButton(id);
+  }
+
+  return(
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClick(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
